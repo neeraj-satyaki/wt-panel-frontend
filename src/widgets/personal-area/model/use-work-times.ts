7c14c0d156872042e11a7ccf136ccbe35cc42a46
Nikita <aruisialalin@yandex.ru>
2023-12-05 17:59:33 +0300

    Исправлен баг с отправкой запрос о времени пользователя без startDate

--- src/widgets/personal-area/model/use-work-times.ts
+++ src/widgets/personal-area/model/use-work-times.ts
@@ -2,16 +2,20 @@ import { useGetTimewWork } from '@/entities/user/queries'
 import { useEffect, useState } from 'react'
 
 export function useWorkTimes(userId: string) {
-  const [currentDate, setCurrentDate] = useState<Date>(new Date())
-  const [startDate, setStartDate] = useState<string>('')
-  const [endDate, setEndDate] = useState<string>('')
-
   const formatDate = (date: Date): string => {
     const year = date.getFullYear()
     const month = String(date.getMonth() + 1).padStart(2, '0')
     const day = String(date.getDate()).padStart(2, '0')
     return `${day}.${month}.${year}`
   }
+
+  const [currentDate, setCurrentDate] = useState<Date>(new Date())
+  const [startDate, setStartDate] = useState<string>(
+    formatDate(new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)),
+  )
+  const [endDate, setEndDate] = useState<string>(
+    formatDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)),
+  )
 
   useEffect(() => {
     const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)
