d1b2c5cfd61dc395c5dd3c0baa0daf9eea94666b
Nikita <aruisialalin@yandex.ru>
2023-12-12 17:43:35 +0300

    refactor:измененено модальное окно в каталоге при поиске через qrcode

--- src/features/catalog/ui/catalog.tsx
+++ src/features/catalog/ui/catalog.tsx
@@ -2,14 +2,20 @@ import { UiHeading } from '@/shared/ui/components/ui-heading'
 import { SearchPanel } from './search-panel'
 import { ListProducts } from './list-products/list-products'
 import { useListProducts } from '../model/use-list-products'
-import { IconQrCode } from '@/shared/ui/icons/icon-qr-code'
 import { useQrCodeScanner } from '../model/qr-code-scanner'
-import { Suspense, lazy } from 'react'
-import { UiPageSpinner } from '@/shared/ui/components/ui-page-spinner'
 import { Filters } from './filters'
 import { Button } from '@/shared/ui/components/ui/button'
 
-const ScannerFindProduct = lazy(() => import('./scanner-find-product'))
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/shared/ui/components/ui/dialog'
+import { Html5QrcodePlugin } from '@/shared/lib/lib-html5-qr-scanner'
 
 export function Catalog() {
   const products = useListProducts()
@@ -17,19 +23,30 @@ export function Catalog() {
 
   return (
     <div className="flex flex-col gap-4">
-      {scanner.showScanner && (
-        <Suspense fallback={<UiPageSpinner />}>
-          <ScannerFindProduct
-            close={scanner.closeScanner}
-            successScan={scanner.handleSuccessScan}
-          />
-        </Suspense>
-      )}
       <div className="flex justify-between">
         <UiHeading level={'1'}>Каталог товаров</UiHeading>
-        <Button variant="outline" className="p-2" onClick={() => scanner.openScanner()}>
-          <IconQrCode />
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="primary">Поиск детали по qr коду</Button>
+          </DialogTrigger>
+          <DialogContent className="max-w-[800px] w-full">
+            <DialogHeader>
+              <DialogTitle>Отсканируйте деталь</DialogTitle>
+            </DialogHeader>
+            <Html5QrcodePlugin
+              onSuccessScan={(decodeText: string) =>
+                scanner.handleSuccessScan(decodeText)
+              }
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="button" variant="secondary">
+                  Закрыть
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
 
       <SearchPanel
