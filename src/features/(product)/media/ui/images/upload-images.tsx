06501d4111caa6851584346c6d3c1e6bd62e31f4
Nikita <aruisialalin@yandex.ru>
2023-12-20 17:51:51 +0300

    fix bag: исправлен баг с добавлением фотографий к товару (На телефона не работало)

--- src/features/(product)/media/ui/images/upload-images.tsx
+++ src/features/(product)/media/ui/images/upload-images.tsx
@@ -1,49 +1,33 @@
-import { useUploadImage } from '@/entities/products/api'
-import { useSessionQuery } from '@/entities/session'
+import { UiSpinner } from '@/shared/ui/components/ui-spinner'
 import { Button } from '@/shared/ui/components/ui/button'
-import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import { Input } from '@/shared/ui/components/ui/input'
+import { useSessionQuery } from '@/entities/session'
+import { useUploadImages } from '@/widgets/product/model/use-upload-images'
 
 type Props = {
   id: string
 }
 
 export function UploadForm({ id }: Props) {
-  const { register, handleSubmit } = useForm()
   const session = useSessionQuery()
-  const uploadImagesMutation = useUploadImage()
-
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    const photosArray: Blob[] = Array.from(data.photos)
-    uploadImagesMutation.mutate({
-      productId: id,
-      files: photosArray,
-      userId: session.data?.id,
-      username: session.data?.name,
-    })
-  }
+  const uploadImages = useUploadImages(id, session.data?.id, session.data?.name)
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
-      <div>
-        <label
-          htmlFor="photos"
-          className="border border-gray-200 flex justify-center items-center h-10 rounded-lg"
-        >
-          Выберите фотографии:
-        </label>
-        <input
-          className="hidden"
+    <div className="text-sm">
+      <form
+        onSubmit={uploadImages.handleSubmit}
+        className="flex gap-2 flex-col 430:flex-row"
+      >
+        <Input
           type="file"
-          id="photos"
-          accept="image/*"
-          multiple
-          {...register('photos')}
+          multiple={true}
+          required={true}
+          {...uploadImages.register('files', {})}
         />
-      </div>
-
-      <div>
-        <Button type="submit">Загрузить</Button>
-      </div>
-    </form>
+        <Button variant="default" type="submit" disabled={uploadImages.isPending}>
+          {uploadImages.isPending ? <UiSpinner /> : 'Добавить'}
+        </Button>
+      </form>
+    </div>
   )
 }
